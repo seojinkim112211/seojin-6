b191e54cf3f2af9b7a7498f6f0a4e0fe5be89530
kimseojin <zzin2990@gmail.com>
2023-08-16 15:16:15 +0900

    [feat] #6 내정보 스토리보드 구성

--- NBCamp/Presentation/Profile/ViewController/ProfileViewController.swift
+++ NBCamp/Presentation/Profile/ViewController/ProfileViewController.swift
@@ -17,5 +17,25 @@ class ProfileViewController: UIViewController {
 	private func setTitle() {
 		title = "내 정보"
 	}
+    
+    class ViewController: UIViewController{
+        let timerSelector:Selector = #selector(ViewController.updateTime)
+        let interval = 1.0
+        var count = 0
+        @IBOutlet weak var IblTimer: UILabel!
+        
+        override func viewDidLoad(){
+            super.viewDidLoad()
+            
+            Timer.scheduledTimer(timeInterval: interval, target: self, selector: timerSelector, userInfo: nil, repeats: true)
+        }
+        
+        @objc func updateTime(){
+            IblTimer.text = String(count)
+            count = count + 1 
+        }
+        
+    }
+    
 
 }
